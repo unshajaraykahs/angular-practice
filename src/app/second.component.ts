da1522aca0e7ea3d8dfd6a539a02869f93879b10
Rajashnu <rajashnushakya@gmail.com>
2024-10-23 23:53:56 +0545

    ..

--- src/app/second.component.ts
+++ src/app/second.component.ts
@@ -1,10 +1,10 @@
 import { Component } from "@angular/core";
+
 @Component({
     standalone:true,
     selector:'sirname',
-    template: '<h2>Shakya</h2>'
+    template: '<h2>hakya</h2>'
 })
-
 export class SecondComponent{
 
 }
